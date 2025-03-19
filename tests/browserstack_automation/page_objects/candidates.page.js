2ef6e35231759da2b842cc1f742f4e3054df5f68
Nadia Vaida <nadia@Nadias-MBP.hsd1.ca.comcast.net>
2025-03-19 11:19:43 -0700

    v-995P1

--- tests/browserstack_automation/page_objects/candidates.page.js
+++ tests/browserstack_automation/page_objects/candidates.page.js
@@ -1,10 +1,11 @@
-import { $, $$ } from '@wdio/globals';
+import { $, $$, driver } from '@wdio/globals';
 import Page from './page';
 import TopNavigation from './topnavigation';
 
 class CandidatesPage extends Page {
   async load () {
     await super.open('');
+    await driver.maximizeWindow();
     await TopNavigation.toggleCandidatesTab();
   }
 
@@ -23,6 +24,23 @@ class CandidatesPage extends Page {
   get CandidateCardList () {
     return $$("//div[contains(@id,'cardForListBodyWrapper')]");
   }
+
+  get CandidateSupportButtonList () {
+    return $$("button[id*='itemActionBarSupportButton'][id*='desktop']");
+  }
+
+  get supportTooltip () {
+    return $('div#supportButtonTooltip');
+  }
+
+  get opposeTooltip () {
+    return $('div#opposeButtonTooltip');
+  }
+
+  get scrollRight () {
+    return $('div#candidateRightArrowDesktop');
+  }
+
 
   async getCandidateCardCandidateName(cardId) {
     const candidate = await $(`//div[@id='${cardId}']//a[@id='candidateCardDisplayName' or @id='representativeCardDisplayName']`);
@@ -51,6 +69,22 @@ class CandidatesPage extends Page {
     const candidateOffice = await $(`//div[@id='${cardId}']//div[contains(@class,'OfficeNameWrapper')]/div`);
     const officeText = candidateOffice.getText();
     return officeText;
+  }
+
+  async getCandidateCardChoose (cardId) {
+    return  $(`div#${cardId} button[id*='itemActionBarSupport'][id*='desktop']`);
+  }
+
+  async getCandidateCardOppose (cardId) {
+    return  $(`div#${cardId} button[id*='itemActionBarOppose'][id*='desktop']`);
+  }
+
+  async getCandidateCardHelpWinButton (cardId) {
+    return $(`div#${cardId} button[id*='itemActionBarHelpThemWinButton'][id*='desktop']`);
+  }
+
+  async getCandidateCardHelpDefeatButton (cardId) {
+    return $(`div#${cardId} button[id*='itemActionBarHelpDefeatButton'][id*='desktop']`);
   }
 }
 export default new CandidatesPage();
--- tests/browserstack_automation/specs/CandidatesPage.js
+++ tests/browserstack_automation/specs/CandidatesPage.js
@@ -1,7 +1,8 @@
 /* eslint-disable no-await-in-loop */
 /* eslint-disable no-use-before-define */
-import { driver } from '@wdio/globals';
+import { driver, browser, expect } from '@wdio/globals';
 import CandidatesPage from '../page_objects/candidates.page';
+import ReadyPage from '../page_objects/ready.page';
 
 const testDataPath = 'tests/browserstack_automation/testDataForScripts/';
 const fs = require('fs');
@@ -12,11 +13,45 @@ const waitTime = 8000;
 /* eslint-disable no-undef */
 // This eslint-disable turns off warnings for describe() and it()
 
+beforeEach(async () => {
+  //await ReadyPage.load();
+  await CandidatesPage.load();
+  await driver.pause(waitTime);
+  //if (await browser.capabilities.browserName === 'safari') {
+    //await browser.execute(() => {
+      //  document.cookie.split(";").forEach((c) => {
+        //    document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/");
+        //});
+    //});
+//} else {
+ //   await browser.deleteCookies();
+//}
+
+
+  //await browser.deleteCookies();
+ // await CandidatesPage.load();
+//  await driver.pause(waitTime);
+ 
+});
+
+afterEach (async () => {
+  //await browser.deleteCookies();
+  if (await browser.capabilities.browserName === 'safari') {
+    await browser.execute(() => {
+        document.cookie.split(";").forEach((c) => {
+            document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/");
+        });
+    });
+} else {
+    await browser.deleteCookies();
+}
+  });
+
 describe('Candidates Page', () => {
   // Candidates_001
   it('verifyAllStateNamesPresentforChooseState', async () => {
-    await CandidatesPage.load();
-    await driver.pause(waitTime);
+   // await CandidatesPage.load();
+    //await driver.pause(waitTime);
     await CandidatesPage.stateSelect.click();
     const options = await CandidatesPage.stateSelectOptions;
     const expectedStates = readTestDataStates('all');
@@ -38,7 +73,7 @@ describe('Candidates Page', () => {
     it('verifyTitleWhenStateSelected', async () => {
       const titleStr = ' Candidates - WeVote';
       console.log(`Running verifyTitleWhenStateSelected -> Using sate: ${state}`);
-      await CandidatesPage.load();
+      //await CandidatesPage.load();
       await CandidatesPage.stateSelect.selectByVisibleText(state);
       await driver.pause(waitTime);
       const expectedTitle = state + titleStr;
@@ -53,8 +88,8 @@ describe('Candidates Page', () => {
   stateNamesRandomTC3.forEach((state) => {
     it('verifyHeadersMatchPossibleHeaders', async () => {
       console.log(`Running verifyHeadersMatchPossibleHeaders -> Using sate: ${state}`);
-      CandidatesPage.load();
-      await driver.pause(waitTime);
+      //CandidatesPage.load();
+      //await driver.pause(waitTime);
       await CandidatesPage.stateSelect.selectByVisibleText(state);
       await driver.pause(waitTime);
       const actualHeaders = await CandidatesPage.pageHeaders;
@@ -71,8 +106,8 @@ describe('Candidates Page', () => {
   stateNamesRandomTC4.forEach((state) => {
     it('verifyMandatoryHeaderPresent', async () => {
       console.log(`Running verifyMandatoryHeaderPresent -> Using sate: ${state}`);
-      CandidatesPage.load();
-      await driver.pause(waitTime);
+      //CandidatesPage.load();
+      //await driver.pause(waitTime);
       await CandidatesPage.stateSelect.selectByVisibleText(state);
       await driver.pause(waitTime);
       const actualHeaders = await CandidatesPage.pageHeaders;
@@ -105,8 +140,8 @@ describe('Candidates Page', () => {
     const errors = [];
     it('verifyCandidateCardHasSectionsDisplayed', async () => {
       console.log(`Running verifyCandidateCardHasSectionsDisplayed -> Using sate: ${state}`);
-      CandidatesPage.load();
-      await driver.pause(waitTime);
+      //CandidatesPage.load();
+      //await driver.pause(waitTime);
       await CandidatesPage.stateSelect.selectByVisibleText(state);
       await driver.pause(waitTime);
       const candidateCards = await CandidatesPage.CandidateCardList;
@@ -138,6 +173,124 @@ describe('Candidates Page', () => {
     });
   });
 
+  // Candidates_010,andidates_011
+  it('verifyChooseOpposeCandidateHoverMessage @WV-995', async () => {
+    await CandidatesPage.stateSelect.selectByVisibleText('Hawaii');
+    const stateNameRandomTC10 = readTestDataStates('random', 1)[0];
+    console.log(`Running verifyChooseOpposeCandidateHoverMessage using state ${stateNameRandomTC10}`);
+    await CandidatesPage.stateSelect.selectByVisibleText(stateNameRandomTC10);
+    await driver.pause(waitTime);
+    const cardId = await getCandidateCardId();
+    const chooseButton = await CandidatesPage.getCandidateCardChoose(cardId);
+    const opposeButton = await CandidatesPage.getCandidateCardOppose(cardId);
+    await driver.executeScript("arguments[0].scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'center' });", [chooseButton]);
+    await driver.executeScript("arguments[0].dispatchEvent(new MouseEvent('mouseover', { bubbles: true }));", [chooseButton]);
+    await driver.waitUntil(async () => CandidatesPage.supportTooltip.isDisplayed(), { timeout: 5000, timeoutMsg: 'Tooltip did not appear in time' });
+    const supportTooltipText = await CandidatesPage.supportTooltip.getText();
+    console.log(`Displayed Support Tooltiptext: ${supportTooltipText}`);
+    const candidateName = await CandidatesPage.getCandidateCardCandidateName(cardId);
+    const expectedSupportTooltipText = (readTooltipsText('SupportAdd')).replace('<Candidate>', candidateName);
+    console.log(`Expected text: ${expectedSupportTooltipText}`);
+    assert.equal(supportTooltipText, expectedSupportTooltipText);
+    await driver.executeScript("arguments[0].dispatchEvent(new MouseEvent('mouseover', { bubbles: true }));", [opposeButton]);
+    await driver.waitUntil(async () => CandidatesPage.opposeTooltip.isDisplayed(), { timeout: 5000, timeoutMsg: 'Tooltip did not appear in time' });
+    const opposeTooltipText = (await CandidatesPage.opposeTooltip.getText());
+    console.log(`Displayed Oppose Tooltiptext: ${opposeTooltipText}`);
+    const expectedOpposeTooltipText = (readTooltipsText('OpposeAdd')).replace('<Candidate>', candidateName);
+    console.log(`Expected text: ${expectedOpposeTooltipText}`);
+    assert.equal(opposeTooltipText, expectedOpposeTooltipText);
+  });
+
+  it('verifyChooseCandidateButtonClick @WV-995', async () => {
+    await CandidatesPage.stateSelect.selectByVisibleText('Hawaii');
+    const stateNameRandomTC10 = readTestDataStates('random', 1)[0];
+    console.log(`Running verifyChooseCandidateButtonClick using state ${stateNameRandomTC10}`);
+    await CandidatesPage.stateSelect.selectByVisibleText(stateNameRandomTC10);
+    await driver.pause(waitTime);
+    const cardId = await getCandidateCardId();
+    const chooseButton = await CandidatesPage.getCandidateCardChoose(cardId);
+    const opposeButton = await CandidatesPage.getCandidateCardOppose(cardId);
+    await driver.executeScript("arguments[0].scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'center' });", [chooseButton]);
+    await chooseButton.click();
+    await driver.pause(waitTime);
+    await expect(chooseButton).not.toHaveText('Choose');
+    await expect(opposeButton).not.toBePresent();
+    await expect(chooseButton).toHaveText('Chosen');
+    const helpWinButton = await CandidatesPage.getCandidateCardHelpWinButton(cardId);
+    await expect(helpWinButton).toBeDisplayed();
+    const expectedHelpWinText = readTooltipsText('HelpWinText');
+    await expect(helpWinButton).toHaveText(expectedHelpWinText);
+  });
+
+  it('verifyOpposeCandidateButtonClick @WV-995', async () => {
+    await CandidatesPage.stateSelect.selectByVisibleText('Hawaii');
+    const stateNameRandomTC10 = readTestDataStates('random', 1)[0];
+    console.log(`Running verifyOpposeCandidateButtonClick using state ${stateNameRandomTC10}`);
+    await CandidatesPage.stateSelect.selectByVisibleText(stateNameRandomTC10);
+    await driver.pause(waitTime);
+    const cardId = await getCandidateCardId();
+    const chooseButton = await CandidatesPage.getCandidateCardChoose(cardId);
+    const opposeButton = await CandidatesPage.getCandidateCardOppose(cardId);
+    await driver.executeScript("arguments[0].scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'center' });", [chooseButton]);
+    await opposeButton.click();
+    await expect(chooseButton).not.toBePresent();
+    await expect(opposeButton).not.toHaveText('Oppose');
+    await expect(opposeButton).toHaveText('Opposed');
+    const helpDefeatButton = await CandidatesPage.getCandidateCardHelpDefeatButton(cardId);
+    await expect(helpDefeatButton).toBeDisplayed();
+    const expectedHelpDefeatText = readTooltipsText('HelpDefeatText');
+    await expect(helpDefeatButton).toHaveText(expectedHelpDefeatText);
+  });
+
+  it('verifyChoosenCandidateHoverMessage @WV-995', async () => {
+    await CandidatesPage.stateSelect.selectByVisibleText('Hawaii');
+    const stateNameRandomTC10 = readTestDataStates('random', 1)[0];
+    console.log(`Running verifyChoosenCandidateHoverMessage using state ${stateNameRandomTC10}`);
+    await CandidatesPage.stateSelect.selectByVisibleText(stateNameRandomTC10);
+    await driver.pause(waitTime);
+    const cardId = await getCandidateCardId();
+    const chooseButton = await CandidatesPage.getCandidateCardChoose(cardId);
+    const opposeButton = await CandidatesPage.getCandidateCardOppose(cardId);
+    await driver.executeScript("arguments[0].scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'center' });", [chooseButton]);
+    await chooseButton.click();
+    await driver.pause(waitTime);
+    await CandidatesPage.scrollRight.moveTo();
+    await driver.executeScript("arguments[0].dispatchEvent(new MouseEvent('mouseover', { bubbles: true }));", [chooseButton]);
+    await driver.waitUntil(async () => CandidatesPage.supportTooltip.isDisplayed(), { timeout: 5000, timeoutMsg: 'Tooltip did not appear in time' });
+    const tooltipText = await CandidatesPage.supportTooltip.getText();
+    const candidateName = await CandidatesPage.getCandidateCardCandidateName(cardId);
+    const expectedTooltipText = readTooltipsText('SupportRemove').replace('<Candidate>', candidateName);
+    await expect(tooltipText).toEqual(expectedTooltipText);
+    await chooseButton.click();
+    await expect(chooseButton).toHaveText('Choose');
+    await expect(opposeButton).toBePresent();
+    await expect(opposeButton).toHaveText('Oppose');
+  });
+
+  it('verifyOpposedCandidateHoverMessage @WV-995', async () => {
+    await CandidatesPage.stateSelect.selectByVisibleText('Hawaii');
+    const stateNameRandomTC10 = readTestDataStates('random', 1)[0];
+    console.log(`Running verifyOpposedCandidateHoverMessage using state ${stateNameRandomTC10}`);
+    await CandidatesPage.stateSelect.selectByVisibleText(stateNameRandomTC10);
+    await driver.pause(waitTime);
+    const cardId = await getCandidateCardId();
+    const chooseButton = await CandidatesPage.getCandidateCardChoose(cardId);
+    const opposeButton = await CandidatesPage.getCandidateCardOppose(cardId);
+    await driver.executeScript("arguments[0].scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'center' });", [chooseButton]);
+    await opposeButton.click();
+    await CandidatesPage.scrollRight.moveTo();
+    await driver.executeScript("arguments[0].dispatchEvent(new MouseEvent('mouseover', { bubbles: true }));", [opposeButton]);
+    await driver.waitUntil(async () => CandidatesPage.opposeTooltip.isDisplayed(), { timeout: 5000, timeoutMsg: 'Tooltip did not appear in time' });
+    const tooltipText = (await CandidatesPage.opposeTooltip.getText());
+    const candidateName = await CandidatesPage.getCandidateCardCandidateName(cardId);
+    const expectedTooltipText = readTooltipsText('OpposeRemove').replace('<Candidate>', candidateName);
+    await expect(tooltipText).toEqual(expectedTooltipText);
+    await opposeButton.click();
+    await expect(chooseButton).toBePresent();
+    await expect(chooseButton).toHaveText('Choose');
+    await expect(opposeButton).toBePresent();
+    await expect(opposeButton).toHaveText('Oppose');
+  });
 
   // read All Possible Headers from candidatesPage_TC001.json
   function readTestDataAllPossibleHeaders () {
@@ -166,5 +319,26 @@ describe('Candidates Page', () => {
       }
     }
     return testStates;
+  }
+
+  // read tooltips text from candidatesPage_TDTooltips.json
+  function readTooltipsText (type) {
+    const jsonObjH = JSON.parse(fs.readFileSync(`${testDataPath}candidatesPage_TDTooltips.json`));
+    const text = jsonObjH[0][type];
+    return text;
+  }
+
+  async function getCandidateCardId () {
+    const candidateCards = await CandidatesPage.CandidateCardList;
+    let selCard =  0;
+    if (candidateCards.length >= 3) {
+      selCard =  1;
+    } else {
+      selCard = candidateCards.length - 1;
+    }
+    const candidateCardRandom = candidateCards[selCard];
+    await driver.waitUntil(async () =>  !(await candidateCardRandom.getAttribute('id')).includes('Loading'), { timeout: 4000, timeoutMsg: 'Card Data not Loaded within expected duration of 4 seconds.' });
+    const candidateCardId = await candidateCardRandom.getAttribute('id');
+    return candidateCardId;
   }
 });
